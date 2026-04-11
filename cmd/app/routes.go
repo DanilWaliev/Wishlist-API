6e105c0df93fc5dc621614f838a40a7a2e1e03c0
Danil Valiev <dnlvlv39@gmail.com>
2026-04-11 18:07:29 +0300

    feat: добавлены эндпоинты для регистрации и входа

--- cmd/app/routes.go
+++ cmd/app/routes.go
@@ -5,7 +5,8 @@ import "net/http"
 func routes(h *HTTPHandler) *http.ServeMux {
 	mux := http.NewServeMux()
 
-	// тут будут эндпоинты
+	mux.HandleFunc("POST /register", h.authHandler.Register)
+	mux.HandleFunc("POST /login", h.authHandler.Login)
 
 	return mux
 }
